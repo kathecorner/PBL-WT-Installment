33e7b4a92e2eb402483474131278658b8d855662
kathecorner <40414604+kathecorner@users.noreply.github.com>
2022-08-29 16:09:26 +0900

    Update action.php

--- action.php
+++ action.php
@@ -45,7 +45,7 @@ unset($payments_data['amount']);
 $final_payment_data = array_merge($payments_data, $additional_data);
 
 $curl_http_header = array(
-    "X-API-Key: QEyhmfxL4PJahZCw0m/n3Q5qf3VaY9UCJ1+XWZe9W27jmlZiv4PD4jhfNMofnLr2K5i8/0QwV1bDb7kfNy1WIxIIkxgBw==-lUKXT9IQ5GZ6d6RH4nnuOG4Bu//eJZxvoAOknIIddv4=-<anpTLkW{]ZgGy,7",
+    "X-API-Key: AQEyhmfxL4PJahZCw0m/n3Q5qf3VaY9UCJ1+XWZe9W27jmlZiv4PD4jhfNMofnLr2K5i8/0QwV1bDb7kfNy1WIxIIkxgBw==-lUKXT9IQ5GZ6d6RH4nnuOG4Bu//eJZxvoAOknIIddv4=-<anpTLkW{]ZgGy,7",
     //↑ここにAPIキーを貼り付けてください。例："X-API-Key: hogehogehogehoge_my_api_key",
     "Content-Type: application/json"
 );
